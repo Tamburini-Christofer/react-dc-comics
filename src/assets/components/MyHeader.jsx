325745302991b8a1421becbeaee56e8c7c588214
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-24 15:20:33 +0200

    Creazione componente Header

---
+++ src/assets/components/MyHeader.jsx
@@ -0,0 +1,27 @@
+const MyHeader = () => {
+    return (
+        <>
+        <header>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default MyHeader;
