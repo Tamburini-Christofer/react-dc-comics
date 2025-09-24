0254d76edabc9f60b29022fad6e3f4c629610934
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-24 19:14:02 +0200

    Creazione Footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,13 +1,21 @@
-const MyFooter= () => {
-    return (
-        <>
-            <div className="sfondoFooter">   
-                <div></div>
-                <div></div>
-            </div>
-         
-        </>
-    )
-}
+const MyFooter = () => {
+  return (
+    <>
+      <div className="sfondoFooter">
+        <div className="principalContainer flex">
+          <button className="signBtn">SIGN-UP NOW!</button>
+          <div>
+            <span className="followText">FOLLOW US</span>
+            <a href=""><img src="../../img/footer-facebook.png" alt="Icona Facebook" /></a>
+            <a href=""><img src="../../img/footer-twitter.png" alt="Icona X" /></a>
+            <a href=""><img src="../../img/footer-youtube.png" alt="Icona Youtube" /></a>
+            <a href=""><img src="../../img/footer-pinterest.png" alt="Icona Pinterest" /></a>
+            <a href=""><img src="../../img/footer-periscope.png" alt="Icona Periscope" /></a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default MyFooter;
